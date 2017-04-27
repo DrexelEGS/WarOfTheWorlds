95d963733799c45c386c5eea45658fa0e73582d6
KARISHMA CHANGLANI <KARISHMA CHANGLANI>
2017-04-27 11:22:25 -0400

    First commit of SuperCollider and Android integration

---
+++ TestingSuperCollider/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,12 @@
+#include <jni.h>
+#include <string>
+#include "supercollider/SCDoc/SCDoc.h"
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_example_karis_testingsupercollider_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
