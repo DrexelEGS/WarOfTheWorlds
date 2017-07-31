1334d7e1522ceec374077635df7649778a62d7ba
strank <strank@strank.info>
2017-07-31 14:03:25 -0400

    Change bufSizeFrames in SCAudio, so it's identical to what's used in NativeAudioTests and corresponds to the comment just above it

--- Sensors2OSC/SuperCollider-library/src/main/java/net/sf/supercollider/android/SCAudio.java
+++ Sensors2OSC/SuperCollider-library/src/main/java/net/sf/supercollider/android/SCAudio.java
@@ -31,7 +31,7 @@ public class SCAudio extends Thread {
 	// bufSizeFrames (size of audio buffer passed in from android) 
 	//  must be a multiple of 64 since scsynth's internal block length is unchanged from its default of 64.
 	// 64*16 was OK for 11kHz.
-	final int bufSizeFrames = 64*8;
+	final int bufSizeFrames = 64*16;
 	final int shortsPerSample = 1; // this is tied to what the NDK code does to pass audio to scsynth, can't change easily.
 	final int bufSizeShorts = bufSizeFrames * numOutChans * shortsPerSample; 
 
