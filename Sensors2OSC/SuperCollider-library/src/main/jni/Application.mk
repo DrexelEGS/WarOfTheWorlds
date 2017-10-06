962204fbfe4276c5c6c2700ff767fb799930049b
strank <strank@strank.info>
2017-10-06 14:31:42 -0400

    Generate arm64-v8a files for native libraries. Needed for the ABI in newer devices.

--- Sensors2OSC/SuperCollider-library/src/main/jni/Application.mk
+++ Sensors2OSC/SuperCollider-library/src/main/jni/Application.mk
@@ -1,4 +1,4 @@
 #use gnustl_shared STL library
 APP_STL := gnustl_shared
 # Build ARMv5TE, ARMv7-A and x86 machine code.
-APP_ABI := armeabi armeabi-v7a x86
+APP_ABI := armeabi armeabi-v7a x86 arm64-v8a
