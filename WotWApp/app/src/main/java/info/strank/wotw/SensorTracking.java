13680c9193bcbe0067e1f7c8acf1d922ab9897a7
strank <strank@strank.info>
2017-10-26 01:58:53 -0400

    Add a Kalman Filter to location tracking, should somewhat smooth out erratic GPS data.

--- WotWApp/app/src/main/java/info/strank/wotw/SensorTracking.java
+++ WotWApp/app/src/main/java/info/strank/wotw/SensorTracking.java
@@ -40,6 +40,8 @@ public class SensorTracking {
     public LatLng currentLocation = exciteLocation;
     public float currentBearing = 0;
 
+    private KalmanLatLong kalmanLatLong = new KalmanLatLong(3f); // for filtering GPS, moving 3m/s
+
 
     public void saveStateToPrefs(SharedPreferences.Editor editor) {
         editor.putInt("location_no", location_no);
@@ -57,8 +59,10 @@ public class SensorTracking {
     }
 
     public double updateDistance(Location location) {
-        double latitude = location.getLatitude();
-        double longitude = location.getLongitude();
+        kalmanLatLong.Process(location.getLatitude(), location.getLongitude(),
+                location.getAccuracy(), location.getTime());
+        double latitude = kalmanLatLong.get_lat();
+        double longitude = kalmanLatLong.get_lng();
         String debugStr = "GPS Location Changed. Lat: " + latitude + " Lng: " + longitude;
         if ((latitude > MIN_LAT && latitude < MAX_LAT) && (longitude > MIN_LNG && longitude < MAX_LNG)) {
             currentLocation = new LatLng(latitude, longitude);
@@ -87,22 +91,19 @@ public class SensorTracking {
 
     public void updateBearing(SensorEvent event){
         float azimut = 0;
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             mGravity = event.values;
-
-        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+        }
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
             mGeomagnetic = event.values;
-
+        }
         if (mGravity != null && mGeomagnetic != null) {
             float R[] = new float[9];
             float I[] = new float[9];
-
             if (SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic)) {
-
                 // orientation contains azimut, pitch and roll
                 float orientation[] = new float[3];
                 SensorManager.getOrientation(R, orientation);
-
                 azimut = orientation[0];
             }
         }
@@ -119,5 +120,86 @@ public class SensorTracking {
 
     public LatLng getTargetLocation() {
         return targetLocations[location_no];
+    }
+
+    /**
+     * Simplified Kalman filtering for latitude/longitude values with timestamp and accuracy
+     * for filtering/smoothing GPS data. Courtesy of
+     * https://stackoverflow.com/questions/1134579/smooth-gps-data
+     */
+    public class KalmanLatLong {
+        private final float MinAccuracy = 1;
+
+        private float Q_metres_per_second;
+        private long TimeStamp_milliseconds;
+        private double lat;
+        private double lng;
+        private float variance; // P matrix.  Negative means object uninitialised.  NB: units irrelevant, as long as same units used throughout
+
+        public KalmanLatLong(float Q_metres_per_second) {
+            this.Q_metres_per_second = Q_metres_per_second;
+            variance = -1;
+        }
+
+        public long get_TimeStamp() {
+            return TimeStamp_milliseconds;
+        }
+
+        public double get_lat() {
+            return lat;
+        }
+
+        public double get_lng() {
+            return lng;
+        }
+
+        public float get_accuracy() {
+            return (float) Math.sqrt(variance);
+        }
+
+        public void SetState(double lat, double lng, float accuracy, long TimeStamp_milliseconds) {
+            this.lat = lat;
+            this.lng = lng;
+            variance = accuracy * accuracy;
+            this.TimeStamp_milliseconds = TimeStamp_milliseconds;
+        }
+
+        /// <summary>
+        /// Kalman filter processing for lattitude and longitude
+        /// </summary>
+        /// <param name="lat_measurement_degrees">new measurement of lattidude</param>
+        /// <param name="lng_measurement">new measurement of longitude</param>
+        /// <param name="accuracy">measurement of 1 standard deviation error in metres</param>
+        /// <param name="TimeStamp_milliseconds">time of measurement</param>
+        /// <returns>new state</returns>
+        public void Process(double lat_measurement, double lng_measurement, float accuracy, long TimeStamp_milliseconds) {
+            if (accuracy < MinAccuracy) accuracy = MinAccuracy;
+            if (variance < 0) {
+                // if variance < 0, object is unitialised, so initialise with current values
+                this.TimeStamp_milliseconds = TimeStamp_milliseconds;
+                lat = lat_measurement;
+                lng = lng_measurement;
+                variance = accuracy * accuracy;
+            } else {
+                // else apply Kalman filter methodology
+
+                long TimeInc_milliseconds = TimeStamp_milliseconds - this.TimeStamp_milliseconds;
+                if (TimeInc_milliseconds > 0) {
+                    // time has moved on, so the uncertainty in the current position increases
+                    variance += TimeInc_milliseconds * Q_metres_per_second * Q_metres_per_second / 1000;
+                    this.TimeStamp_milliseconds = TimeStamp_milliseconds;
+                    // TO DO: USE VELOCITY INFORMATION HERE TO GET A BETTER ESTIMATE OF CURRENT POSITION
+                }
+
+                // Kalman gain matrix K = Covarariance * Inverse(Covariance + MeasurementVariance)
+                // NB: because K is dimensionless, it doesn't matter that variance has different units to lat and lng
+                float K = variance / (variance + accuracy * accuracy);
+                // apply K
+                lat += K * (lat_measurement - lat);
+                lng += K * (lng_measurement - lng);
+                // new Covarariance  matrix is (IdentityMatrix - K) * Covarariance
+                variance = (1 - K) * variance;
+            }
+        }
     }
 }
